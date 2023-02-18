787756e9a07fe0822eb382e4159f40663ea03e9e
Joabesv <joabevarjao123@gmail.com>
2023-02-18 20:18:28 -0300

    feat<knex>: start knex in app

---
+++ src/database/connection.ts
@@ -0,0 +1,14 @@
+import { knex as setupKnex, Knex } from 'knex';
+import { config } from '../models/schema/env.schema';
+
+export const knexConfig: Knex.Config = {
+  client: config.DATABASE_CLIENT,
+  connection: config.DATABASE_URL,  
+  searchPath: ['knex', 'public'],
+  migrations: {
+    extension: 'ts',
+    directory: './src/database/migrations',
+  },
+};
+
+export const knex = setupKnex(knexConfig);
