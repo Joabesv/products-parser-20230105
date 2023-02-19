fe902824af6bc4215e230d47efab087c8bcae09d
Joabesv <joabevarjao123@gmail.com>
2023-02-19 17:06:18 -0300

    feat<base-route>: finish base route

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import { app } from './app';
 import { knex } from './database/connection';
 import { config } from './models/schema/env.schema';
+import { formatMemoryUsage } from './utils/formatMemoryUsage';
 
 app.get('/', async (request, reply) => {
   const isConnectionStable = (await knex('products').whereNotNull('id'))
     ? 'Stable'
     : 'Error querying database';
   // TODO: last time cron job was started = will query a FK in schema
-
+  const requestMemUsage = process.memoryUsage().heapUsed;
   await reply.status(200).send({
     msg: 'This is a challenge by coodesh!',
     databaseStatus: isConnectionStable,
-    RamUsage: process.memoryUsage().rss,
+    RamUsage: formatMemoryUsage(requestMemUsage),
   });
 });
 
