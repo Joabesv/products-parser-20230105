e24103e34ef2a64b8d2137caa93c7e78b693b06c
Joabesv <joabevarjao123@gmail.com>
2023-02-19 01:42:52 -0300

    feat<logger> pretty print for logger

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import fastify from 'fastify';
+import { productRoutes } from './modules/products/products.routes';
+import { prettyLogger } from './utils/logger';
 
 export const app = fastify({
-  logger: true,
+  logger: { transport: prettyLogger },
 });
+
+app.register(productRoutes, { prefix: '/products' });
