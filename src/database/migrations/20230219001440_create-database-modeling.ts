966a55038f328ec26afa73ba861534d421637635
Joabesv <joabevarjao123@gmail.com>
2023-02-21 19:05:43 -0300

    feat<migration>: update migration fields

--- src/database/migrations/20230219001440_create-database-modeling.ts
+++ src/database/migrations/20230219001440_create-database-modeling.ts
@@ -3,18 +3,18 @@ import { Knex } from 'knex';
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('products', (table) => {
     table.increments('id', { primaryKey: true });
-    table.enu('column', ['draft', 'trash', 'published']).notNullable();
+    table.enu('status', ['draft', 'trash', 'published']).notNullable();
     table.timestamp('imported_t').defaultTo(knex.fn.now()).notNullable();
     table.text('url').notNullable();
     table.text('creator').notNullable();
-    table.timestamp('created_t').defaultTo(knex.fn.now()).notNullable();
-    table.timestamp('last_modified_t').defaultTo(knex.fn.now()).notNullable();
+    table.integer('created_t');
+    table.integer('last_modified_t');
     table.text('product_name').notNullable();
     table.text('quantity').notNullable();
     table.text('brands').notNullable();
     table.text('categories').notNullable();
     table.text('labels').notNullable();
-    table.text('cities').notNullable();
+    table.text('cities');
     table.text('purchase_places').notNullable();
     table.text('stores').notNullable();
     table.text('ingredients_text').notNullable();
@@ -22,7 +22,7 @@ export async function up(knex: Knex): Promise<void> {
     table.text('serving_size').notNullable();
     table.float('serving_quantity').notNullable();
     table.integer('nutriscore_score').notNullable();
-    table.text('nutriscore_grade').notNullable();
+    table.string('nutriscore_grade');
     table.text('main_category').notNullable();
     table.text('image_url').notNullable();
   });
