a057085752ec16108ba0ebd8ede03c70f94a587a
Joabesv <joabevarjao123@gmail.com>
2023-02-21 19:05:01 -0300

    fix<root>: separate root route

---
+++ src/modules/root.ts
@@ -0,0 +1,18 @@
+import type { FastifyInstance } from 'fastify';
+import { knex } from '../database/connection';
+import { formatMemoryUsage } from '../utils/formatMemoryUsage';
+
+export async function rootRoute(app: FastifyInstance) {
+  app.get('/', async (request, reply) => {
+    const isConnectionStable = (await knex('products').whereNotNull('id'))
+      ? 'Stable'
+      : 'Error querying database';
+    // TODO: last time cron job was started = will query a FK in schema
+    const requestMemUsage = process.memoryUsage().heapUsed;
+    await reply.status(200).send({
+      msg: 'This is a challenge by coodesh!',
+      databaseStatus: isConnectionStable,
+      RamUsage: formatMemoryUsage(requestMemUsage),
+    });
+  });
+}
